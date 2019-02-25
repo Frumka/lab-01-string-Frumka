c0a1d18b433477e6e39be78520e3910d6ca69fdf
atrum <Hekchanel@mail.ru>
2019-02-26 00:35:35 +0300

    Simplified to adsolute garbage'(. Even more

--- source_code/string.cpp
+++ source_code/string.cpp
@@ -30,7 +30,7 @@ String::String(const char *data) {
           }*/
     size_t ind;
     for (ind = 0; data[ind] != 0; ind++) continue;
-    Data = new char[ind];
+    Data = new char[ind + 1];
     for (size_t i = 0; i <= ind; i++) {
         Data[i] = data[i];
     }
